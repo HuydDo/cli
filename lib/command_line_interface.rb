f0f19282510de5484fb310c998cd9a9a833281a1
Huy Do <huyddo40@gmail.com>
2019-07-22 16:11:33 +0000

    added command_line_interface.rb

---
+++ lib/command_line_interface.rb
@@ -0,0 +1,22 @@
+require_relative "../lib/scraper.rb"
+require_relative "../lib/details.rb"
+require 'nokogiri'
+require 'colorize'
+
+class CommandLineInterface
+  def run
+    
+  end
+  
+  def make
+    
+  end
+  
+  def add_attributes
+    
+  end
+  
+  def display
+    
+  end
+end
