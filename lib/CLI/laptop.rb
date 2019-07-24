a0666c701bdbee2e7d75feb5963b19b0fd72be1d
Huy Do <huyddo40@gmail.com>
2019-07-24 16:14:55 +0000

    modified lapto.rb

--- lib/CLI/laptop.rb
+++ lib/CLI/laptop.rb
@@ -1,6 +1,20 @@
 class BestLaptops::Laptop
   attr_accessor :name, :price, :bottom_line, :url 
   
+  @@all = []
+  
+  def initialize
+      @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @all.clear
+  end
+  
   def run
     self.scape_laptops
   end
