4b0aa0a69e587082fdb80c508134aed5a86988e8
Huy Do <huyddo40@gmail.com>
2019-07-25 06:49:23 +0000

    modified cli.rb

--- lib/CLI/cli.rb
+++ lib/CLI/cli.rb
@@ -1,6 +1,14 @@
 require_relative "./scraper.rb"
 require_relative './laptop.rb'
 
+#TODO:
+# until input == 'exit'
+#   case input 
+#   when 
+#   end
+# end
+# open new page if select read more
+
 # class BestLaptops::CLI
 class CLI 
   
@@ -27,23 +35,23 @@ class CLI
   def menu
     input = nil
     while input != 'exit'
-      puts "Please enter number for more information. Type show to display the laptop list. Type exit to quit. "
+      puts "Please enter the number of laptop for more information. Type show to display the laptop list. Type exit to quit. "
       input = gets.strip.downcase
       
-      if input.to_i > 0 
-        laptop = @list[input.to_i - 1]
-        puts laptop
-        puts "Name: #{laptop.name}"
-        puts "  #{laptop.price}"
-        puts "  Description: #{laptop.description}"
-        puts "  Read Review: #{laptop.url}"
+      if input.to_i > 0 and input.to_i <= @list.size
+        selected_laptop(input.to_i)
+        # laptop = @list[input.to_i - 1]
+        # puts "Name: #{laptop.name} - #{laptop.price}"
+        # puts "Review: #{laptop.description}"
+        # puts "  Read Review: #{laptop.url}"
       elsif input == "show"
         list_laptops
       else
-        puts "Have a good day!"
+        puts "Please check your input again."
       end
     end
-      
+    
+   
     # Laptop.all.each do |laptop|
     #   puts "Name: #{laptop.name}"
     #   puts "  #{laptop.price}"
@@ -52,4 +60,11 @@ class CLI
     # end
   end
   
+  def selected_laptop(number)
+    laptop = @list[number - 1]
+    puts "Name: #{laptop.name} - #{laptop.price}"
+    puts "Description: #{laptop.description}"
+    puts "Would you like to read more review?"
+      # puts "  Read Review: #{laptop.url}"
+  end  
 end
