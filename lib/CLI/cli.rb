c6971ec71d6231580384aafca2856d2a109a2ef3
Huy Do <huyddo40@gmail.com>
2019-07-27 04:56:21 +0000

    modified cli.rb

--- lib/CLI/cli.rb
+++ lib/CLI/cli.rb
@@ -15,7 +15,7 @@ class CLI
   def run 
     make_laptops
     list_laptops
-    add_laptop_detail
+    # add_laptop_detail
     menu
   end
   
@@ -44,10 +44,28 @@ class CLI
   def menu
     input = nil
     while input != 'exit'
-      puts "Please enter the number of laptop for more information. Type show to display the laptop list. Type exit to quit. "
+      puts "Enter the number of laptop for more information. Type show to display the list. Type exit to quit. "
       input = gets.strip.downcase
       
       if input.to_i > 0 and input.to_i <= @list.size
+        puts "Name: #{laptop.name} - #{laptop.price}"
+        puts "Description: #{laptop.description}"
+        puts "Would you like to read more?"
+        input = gets.strip
+    
+        if ["Y", "YES"].include?(input.upcase)
+          # content = scrape_laptop_page(laptop_url)
+          # content = @list.content
+          # puts content
+          
+          puts "Pros: #{laptop.pros}"
+          puts "Cons: #{laptop.pros}"
+          puts "Bottom Line: #{laptop.pros}"
+        end
+        puts "Would you like to exit or show the list again?"
+        input = gets.strip
+        
+        
         selected_laptop(input.to_i)
         # laptop = @list[input.to_i - 1]
         # puts "Name: #{laptop.name} - #{laptop.price}"
@@ -76,7 +94,7 @@ class CLI
     puts "Name: #{laptop.name} - #{laptop.price}"
     puts "Description: #{laptop.description}"
     puts "Would you like to read more?"
-    puts = gets.strip
+    input = gets.strip
     
     if ["Y", "YES"].include?(input.upcase)
       # content = scrape_laptop_page(laptop_url)
